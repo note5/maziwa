d9b7bc1e0b636f76122c16abdda03945f2f0ea75
Note5 <note5mn@gmail.com>
2018-07-08 18:31:47 +0300

    created the edit pages for farmer and cow, made the data table crud like

--- src/components/Cows_components/Cows.vue
+++ src/components/Cows_components/Cows.vue
@@ -1,35 +1,44 @@
 <template>
   <div>
+    <!--add cow modal-->
     <v-layout row justify-center>
     <v-dialog v-model="dialog" persistent max-width="700">
       <v-btn slot="activator" color="success" dark>ADD a cow</v-btn>
       <v-card>
         <v-card-title class="justify-center" >
-          <span  class="headline">Cow Details</span>
+          <span  class="headline"><strong> Cow Details</strong></span>
+          <v-spacer></v-spacer>
+         <v-btn flat color="red" @click.native="dialog = false"> <v-icon >highlight_off</v-icon></v-btn>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12>
-                <v-text-field label="Name of cow" v-model="cow_name" required hint="Make the name unique for each cow" persistent-hint></v-text-field>
+                  <h3>Name of Cow: </h3>
+                <v-text-field  v-model="cow_name" required hint="Make the name unique for each cow" persistent-hint></v-text-field>
               </v-flex>
               <v-flex xs12>
-                <v-text-field label="Breed of the Cow" required hint="What is the breed of the Cow?" persistent-hint></v-text-field>
+                <h3>Breed of the Cow: </h3>
+                <v-text-field v-model="breed" required hint="What is the breed of the Cow?" persistent-hint></v-text-field>
               </v-flex>
               <v-flex xs12 >
-                <v-text-field label="Year the cow was born" required hint="Add the year the cow was born"  persistent-hint ></v-text-field>
+                <h3>Year the cow was born: </h3>
+                <v-text-field v-model="year_born"  hint="Add the year the cow was born"  persistent-hint ></v-text-field>
               </v-flex>  
               <v-flex xs12 >
-                <v-text-field label="Weight of the cow" required hint="Add the recent weight in kgs"  persistent-hint ></v-text-field>
+                <h3>Weight of Cow: </h3>
+                <v-text-field  v-model="weight" required hint="Add the recent weight in kgs"  persistent-hint ></v-text-field>
               </v-flex>  
               <v-flex xs12 >
+                <h3>State of the Cow: </h3>
                 <v-select auto v-model="value"  tags ref="value" :items="items" attach chips label="Current state of the cow"
                     required hint="what is the current state of the cow?"  persistent-hint
                     multiple >
                 </v-select>
               </v-flex>  
               <v-flex xs12>
-                <input  type="file" @change="onFileChanged"  @keyup.enter="onUpload">
+                <h3>Picture of the  Cow: </h3>
+                <input  type="file" @change="onFileChanged" >
                   <v-btn color="success" @click="onUpload">Upload!</v-btn>
                    
                   <div class="image-preview" v-if="imageData.length > 0">
@@ -38,7 +47,6 @@
             </div>
               </v-flex>
             </v-layout>
-            <small>*indicates required field</small>
           </v-container>
            
         </v-card-text>
@@ -50,6 +58,64 @@
       </v-card>
     </v-dialog>
   </v-layout>
+  <!--end of add cow modal-->
+  <!--start of edit cow modal-->
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog_edit" persistent max-width="700">
+      <v-card>
+        <v-card-title class="justify-center" >
+          <span  class="headline"><strong> Cow Details</strong></span>
+          <v-spacer></v-spacer>
+         <v-btn flat color="red" @click.native="dialog_edit = false"> <v-icon >highlight_off</v-icon></v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12>
+                <h3>Name of the Cow: </h3>
+                <v-text-field  v-model="cow_name_edit" required hint="Make the name unique for each cow" persistent-hint></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <h3>Breed of the  Cow: </h3>
+                <v-text-field  v-model="breed_edit" required hint="What is the breed of the Cow?" persistent-hint></v-text-field>
+              </v-flex>
+              <v-flex xs12 >
+                <h3>Year the Cow was born: </h3>
+                <v-text-field  v-model="year_born_edit" required hint="Add the year the cow was born"  persistent-hint ></v-text-field>
+              </v-flex>  
+              <v-flex xs12 >
+                <h3>Weight of the Cow: </h3>
+                <v-text-field  v-model="weight_edit" required hint="Add the recent weight in kgs"  persistent-hint ></v-text-field>
+              </v-flex>  
+              <v-flex xs12 >
+                <h3>State of the Cow: </h3>
+                <v-select auto v-model="value_edit"  tags ref="value_edit" :items="items" attach chips 
+                    required hint="what is the current state of the cow?"  persistent-hint
+                    multiple >
+                </v-select>
+              </v-flex>  
+              <v-flex xs12>
+                <input  type="file" @change="onFileChanged" >
+                  <v-btn color="success" @click="onUpload">Upload!</v-btn>
+                   
+                  <div class="image-preview" v-if="imageData_edit.length > 0">
+                   <div><h2>Name: {{cow_name}}</h2></div>
+                <img class="preview mt-2" :src="imageData_edit">
+            </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+           
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog_edit = false">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="print" @click.native="dialog_edit = false">Edit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+  <!-- end of edit cow modal-->
    <v-layout row wrap>
   <v-flex xs12 sm12>
     <v-card class="mt-2 mb-0">
@@ -59,18 +125,18 @@
     <div>
   <v-card>
     <v-card-title>
-      Nutrition
+      Cows
       <v-spacer></v-spacer>
       <v-text-field  v-model="search" append-icon="search" label="Search" single-line hide-details ></v-text-field>
     </v-card-title>
-    <v-data-table :headers="headers" :items="desserts" :search="search"  >
+    <v-data-table :headers="headers" :items="cows" :search="search"  >
       <template slot="items" slot-scope="props">
         <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
+        <td>{{ props.item.weight }}</td>
+        <td>{{ props.item.breed }}</td>
+        <td >{{ props.item.picture }}</td>
+        <td @click="show(props.item)" style="cursor:pointer; margin-left:1px" class="text-xs-right"> <i class="material-icons">border_color</i></td>
+        <td @click="print(props.item.weight)"  class="text-xs-right" style="cursor:pointer" > <i class="material-icons">delete</i></td>
       </template>
       <v-alert slot="no-results" :value="true" color="error" icon="warning">
         Your search for "{{ search }}" found no results.
@@ -96,116 +162,69 @@ export default {
   },
   data() {
     return {
-      cow_name:'',
+      cow_name: "",
       imageData: "",
+      breed:null,
+      year_born:null,
+      weight:null,
+      //edit modal values
+      cow_name_edit: "",
+      imageData_edit: "",
+      breed_edit:null,
+      year_born_edit:null,
+      weight_edit:null,
+      
       selectedFile: null,
       dialog: false,
+      dialog_edit: false,
       items: ["pregnant", "sold", "dead", "slaughtered", "dry", "milked"],
       value: [],
+      value_edit:[],
       search: "",
       headers: [
         {
-          text: "Dessert (100g serving)",
-          align: "left",
+          text: "Name of cow",
           sortable: false,
           value: "name"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        { text: "Weight (Kgs)", value: "weight" },
+        { text: "Breed", value: "breed" },
+        { text: "Picture", value: "picture" 
+        
+        },
+        {text:"Edit",value:"edit"},
+        { text: "Delete", value: "delete" }
       ],
-      desserts: [
+      cows: [
         {
           value: false,
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
+          name: "chelel",
+          weight: 408,
+          breed: "freshian",
+          picture: "https://media.mnn.com/assets/images/2017/01/cow-in-pasture.jpg.838x0_q80.jpg",
+          date: "1/1/2009",
+          delete: "Delete"
         },
         {
           value: false,
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
+          name: "baringo",
+          weight: 345,
+          breed: "ashyre",
+          picture: "http/hshshhs/hdhdd",
+          date: "1/1/2009",
+
+          delete: "Delete"
         },
         {
           value: false,
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          value: false,
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          value: false,
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          value: false,
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          value: false,
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          value: false,
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          value: false,
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          value: false,
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
+          name: "mardadi",
+          weight: 287,
+          breed: "jersey",
+          picture: "https://media.mnn.com/assets/images/2017/01/cow-in-pasture.jpg.838x0_q80.jpg",
+          state:["pregnant","dry"],
+          date: "1/1/2009",
+
+          delete: "Delete"
         }
       ]
     };
@@ -215,31 +234,46 @@ export default {
       this.$router.push(route);
     },
     print() {
-      var i = 0;
-      for (i in this.value) {
-        console.log(this.value[i]);
+      this.value.forEach(i => {
+              console.log(i);
+      });
+    },
+    show(x){
+      this.dialog_edit=true
+      this.cow_name_edit=x.name
+      this.breed_edit=x.breed
+      this.year_born_edit=x.date
+      this.weight_edit=x.weight
+      if(x.state){
+      this.value_edit=x.state
       }
+      this.imageData_edit=x.picture
     },
     onFileChanged(event) {
       const file = event.target.files[0];
       this.selectedFile = file;
-      if(file){
-      this.imageData = URL.createObjectURL(file);
+      if (file) {
+        this.imageData = URL.createObjectURL(file);
       }
     },
-    onUpload() { //debugging purposes
-      console.log('file', this.selectedFile)
+    onUpload() {
+      //debugging purposes
+      console.log("file", this.selectedFile);
       console.log(this.selectedFile);
     }
-  }
+  }      
+
 };
 </script>
 <style scoped>
 img.preview {
-    max-width:300px;
-    max-height:200px;
-    background-color: grey;
-    border: 1px solid #DDD;
-    padding: 3px;
+  max-width: 300px;
+  max-height: 200px;
+  background-color: grey;
+  border: 1px solid #ddd;
+  padding: 3px;
+}
+h3{
+  color: blue;
 }
 </style>
--- src/components/Farmer_components/Farmer.vue
+++ src/components/Farmer_components/Farmer.vue
@@ -1,12 +1,14 @@
 <template>
- <div>
-   <v-layout row justify-center>
-      <v-flex xs12>
-         <v-dialog v-model="dialog" persistent max-width="700">
-            <v-btn slot="activator" color="success" dark>Create A farmer</v-btn>
-            <v-card>
+  <div>
+    <!--add farmer detals-->
+    <v-layout row justify-center>
+          <v-dialog v-model="dialog" persistent max-width="700">
+            <v-btn v-if="!farm_name" slot="activator" color="success" dark>Create A farmer</v-btn>
+               <v-card>
                <v-card-title class="justify-center" >
-                  <span  class="headline">Farmer Details</span>
+                  <span  class="headline"><strong>Farmer Details</strong></span>
+                    <v-spacer></v-spacer>
+                   <v-btn flat color="red" @click.native="dialog = false"> <v-icon >highlight_off</v-icon></v-btn>
                </v-card-title>
                <v-card-text>
                   <v-container grid-list-md>
@@ -42,19 +44,105 @@
                   <v-btn color="green darken-1"  @click.native="dialog = false">Cancel</v-btn>
                   <v-btn color="green darken-1"   @click.native="dialog = false">Submit</v-btn>
                </v-card-actions>
-            </v-card>
-         </v-dialog>
-      </v-flex>
-   </v-layout >
-   <v-layout row wrap>
-   <v-flex xs12>
-      <h1>farmer</h1>
-   </v-flex>
-   </v-layout>
-</div>
- 
-</template>
+               </v-card>
+          </v-dialog>
+    </v-layout>
+    <!-- end of adding farmer details modal -->
 
+    <!--Edit modal-->
+       <v-layout row justify-center>
+          <v-dialog v-model="dialog_Edit" persistent max-width="700">
+               <v-card>
+               <v-card-title class="justify-center" >
+                  <span  class="headline"><strong>Edit Farmer Details</strong></span>
+                    <v-spacer></v-spacer>
+                   <v-btn flat color="red" @click.native="dialog_Edit = false"> <v-icon >highlight_off</v-icon></v-btn>
+               </v-card-title>
+               <v-card-text>
+                  <v-container grid-list-md>
+                     <v-layout wrap>
+                        <v-flex xs12>
+                           <h3>Name of farmer</h3>
+                           <v-text-field label="Name of Farmer" v-model="name" required></v-text-field>
+                        </v-flex>
+                        <v-flex xs12>
+                           <h3>Select County: </h3>
+                           <p v-if="county"> **You selected :{{county}}** </p>
+                           <basic-select :options="options"  :selected-option="counties"    placeholder="select county"
+                              @select="onSelect">
+                           </basic-select>
+                        </v-flex>
+                        <v-flex xs12 >
+                           <h3>Select Subcounty: </h3>
+                           <p v-if="subcounty">** You Selected: {{subcounty}}**</p>
+                           <basic-select :options="options_subcounties"  :selected-option="subcounties"   required placeholder="select subcounty"
+                              @select="onSelect_sub">
+                           </basic-select>
+                        </v-flex>
+                        <v-flex xs12 class="mt-2">
+                           <h3>Enter Size of land</h3>
+                           <v-text-field label="Size of land"  v-model="land_size" required hint="How big is your farm?"  persistent-hint ></v-text-field>
+                        </v-flex>
+                     </v-layout>
+                     <small>* indicates required field</small>
+                  </v-container>
+               </v-card-text>
+               <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="green darken-1"  @click.native="dialog_Edit = false">Cancel</v-btn>
+                  <v-btn color="green darken-1"  @click="submit_edited" @click.native="dialog_Edit = false">edit</v-btn>
+               </v-card-actions>
+               </v-card>
+          </v-dialog>
+    </v-layout>
+    <!--end of edit modal-->
+    <v-layout row wrap>
+      <v-flex xs12 md6 offset-md3>
+      <v-card class="mt-3">
+         <v-container>
+           <v-list-tile-content>
+             <v-list-tile-title class="mb-2">
+               <h2 class="mb-4" ><strong>Farm name:</strong></h2>
+             </v-list-tile-title>
+             <v-list-tile-sub-title class="mb-2">
+               {{farm_name}}
+             </v-list-tile-sub-title>
+           </v-list-tile-content>
+           <v-divider></v-divider>
+           <v-list-tile-content>
+             <v-list-tile-title class="mb-2">
+               <h2><strong>County:</strong></h2>
+             </v-list-tile-title>
+             <v-list-tile-sub-title class="mb-2">
+               {{county_name}}
+             </v-list-tile-sub-title>
+           </v-list-tile-content>
+           <v-divider></v-divider>
+           <v-list-tile-content>
+             <v-list-tile-title class="mb-2">
+               <h2><strong>Subcounty:</strong></h2>
+             </v-list-tile-title>
+             <v-list-tile-sub-title class="mb-2">
+               {{subcounty_name}}
+             </v-list-tile-sub-title>
+           </v-list-tile-content>
+           <v-divider></v-divider>
+           <v-list-tile-content>
+             <v-list-tile-title class="mb-2">
+              <h2><strong>Land Size</strong></h2>
+             </v-list-tile-title>
+             <v-list-tile-sub-title class="mb-2">
+               {{size_of_land}}
+             </v-list-tile-sub-title>
+           </v-list-tile-content>
+           <v-divider></v-divider>
+           <v-btn block color="success" dark @click="edit_farmer">Edit farmer details</v-btn>
+         </v-container>
+      </v-card>  
+      </v-flex>    
+    </v-layout>
+  </div>
+</template>
 <script>
 import { BasicSelect } from "vue-search-select";
 import json from "@/counties.json";
@@ -63,6 +151,7 @@ export default {
   data() {
     return {
       dialog: false,
+      dialog_Edit:false,
       name: null,
       county: null,
       subcounty: null,
@@ -77,7 +166,12 @@ export default {
       subcounties: {
         text: ""
       },
-      searchText: ""
+      searchText: "",
+      //place the farmer data on the card
+      farm_name:'Kapkamuren',
+      county_name:'Baringo',
+      subcounty_name:'Koibatek',
+      size_of_land:'200 acres'
     };
   },
   methods: {
@@ -106,6 +200,14 @@ export default {
     selectOption() {
       // select option from parent component
       this.counties = this.options[0];
+    },
+    //edit farmer details function
+    edit_farmer(){
+      this.dialog_Edit= true
+      console.log(this.dialog)
+    },
+    submit_edited(){
+      console.log("send edited")
     }
   },
   components: {
